4b23225507a859c51da066bdf9e3e50631d6d9f2
cyr <cyr@zolotarev.pp.ua>
2021-03-29 22:02:12 +0300

    settings for media  files

--- Pizzeria_django/settings.py
+++ Pizzeria_django/settings.py
@@ -191,7 +191,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/'
 
 PAGINATE_CHOICES = ['4', '12', '16', '20', '24', '32', '40', '80']
 DEFAULT_PAGINATE = PAGINATE_CHOICES[1]
